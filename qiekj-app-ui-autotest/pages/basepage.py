42afdff9569792e77e96b0d5f4ce35f810d71490
JacStphen <jacstephen@163.com>
2019-11-18 16:23:34 +0800

    project commit

---
+++ qiekj-app-ui-autotest/pages/basepage.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/18 11:52
+# @Author  : Stephen
+# @Email   : jacstephen@163.com
+# @File    : basepage.py
+# @Software: PyCharm
---
+++ qiekj-app-ui-autotest/test_results/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/18 11:48
+# @Author  : Stephen
+# @Email   : jacstephen@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
